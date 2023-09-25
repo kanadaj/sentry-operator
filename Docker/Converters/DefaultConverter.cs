682dfb0d15008fb1ddf8341916aa2cfe5714332f
Daniel Rusznyak <daniel@rusznyak.com>
2023-09-26 00:12:16 +0100

    Add base env vars to all default containers

--- Docker/Converters/DefaultConverter.cs
+++ Docker/Converters/DefaultConverter.cs
@@ -1,7 +1,24 @@
-﻿namespace SentryOperator.Docker.Converters;
+﻿using k8s.Models;
+using SentryOperator.Entities;
+
+namespace SentryOperator.Docker.Converters;
 
 public class DefaultConverter : ContainerConverter
 {
     public override int Priority => -1;
     public override bool CanConvert(string name, DockerService service) => true;
+
+    protected override V1Container GetBaseContainer(string name, DockerService service, SentryDeployment sentryDeployment)
+    {
+        var container = base.GetBaseContainer(name, service, sentryDeployment);
+        
+        
+        container.EnvFrom ??= new List<V1EnvFromSource>();
+        container.EnvFrom.Add(new V1EnvFromSource
+        {
+            SecretRef = new V1SecretEnvSource("sentry-env")
+        });
+
+        return container;
+    }
 }
