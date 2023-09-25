bda89df84b363c704d0aa5a13ce67e2a3e2b1468
Daniel Rusznyak <daniel@rusznyak.com>
2023-09-25 19:03:02 +0100

    More fixes

--- Docker/DockerComposeConverter.cs
+++ Docker/DockerComposeConverter.cs
@@ -218,7 +218,7 @@ internal class DockerComposeConverter
                 {
                     Name = "sentry-cron",
                     Optional = false,
-                    DefaultMode = 420
+                    DefaultMode = 530
                 }
             });
             container.Args = new List<string>
