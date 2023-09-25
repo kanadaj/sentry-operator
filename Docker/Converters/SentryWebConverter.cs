c39e114ee13010b90b67d4e6a4276cb1ed055a4d
Daniel Rusznyak <daniel@rusznyak.com>
2023-09-26 00:09:29 +0100

    Fix web

--- Docker/Converters/SentryWebConverter.cs
+++ Docker/Converters/SentryWebConverter.cs
@@ -1,7 +1,21 @@
-﻿namespace SentryOperator.Docker.Converters;
+﻿using SentryOperator.Docker.Volume;
+using SentryOperator.Entities;
+
+namespace SentryOperator.Docker.Converters;
 
 public class SentryWebConverter : SentryContainerConverter
 {
     public override int Priority => 1;
     public override bool CanConvert(string name, DockerService service) => name == "web";
+    
+    protected override IEnumerable<VolumeRef> GetVolumeData(DockerService service, SentryDeployment sentryDeployment)
+    {
+        var knownNames = new List<string>();
+        foreach(var volume in base.GetVolumeData(service, sentryDeployment))
+        {
+            if(knownNames.Contains(volume.Name)) continue;
+            yield return volume;
+            knownNames.Add(volume.Name);
+        }
+    }
 }
