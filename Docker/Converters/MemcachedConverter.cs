ee99958c1bf5cb1010ad23b4d809cb86d9481fc2
Daniel Rusznyak <daniel@rusznyak.com>
2023-09-26 00:30:40 +0100

    Fix memcached

---
+++ Docker/Converters/MemcachedConverter.cs
@@ -0,0 +1,19 @@
+﻿using k8s.Models;
+using SentryOperator.Entities;
+
+namespace SentryOperator.Docker.Converters;
+
+public class MemcachedConverter : ContainerConverter
+{
+    public override bool CanConvert(string name, DockerService service) => name == "memcached";
+
+    protected override V1Container GetBaseContainer(string name, DockerService service, SentryDeployment sentryDeployment)
+    {
+        var container = base.GetBaseContainer(name, service, sentryDeployment);
+
+        container.LivenessProbe = null;
+        container.ReadinessProbe = null;
+
+        return container;
+    }
+}
