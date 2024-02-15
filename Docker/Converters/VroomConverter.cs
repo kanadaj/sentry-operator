e8285153d01f5cf3c403da0d614a8e62f9098616
Daniel Rusznyak <daniel@rusznyak.com>
2024-02-15 21:07:43 +0000

    Add vroom port

---
+++ Docker/Converters/VroomConverter.cs
@@ -0,0 +1,18 @@
+﻿using k8s.Models;
+using SentryOperator.Entities;
+
+namespace SentryOperator.Docker.Converters;
+
+public class VroomConverter : DefaultConverter
+{
+    protected override V1Container GetBaseContainer(string name, DockerService service, SentryDeployment sentryDeployment)
+    {
+        var container = base.GetBaseContainer(name, service, sentryDeployment);
+        container.Ports ??= new List<V1ContainerPort>();
+        container.Ports.Add(new V1ContainerPort()
+        {
+            ContainerPort = 8085
+        });
+        return container;
+    }
+}
