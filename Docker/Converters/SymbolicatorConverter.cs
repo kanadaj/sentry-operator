469a855578cab0168ac1f57dc93916f3325c4db4
Daniel Rusznyak <daniel@rusznyak.com>
2024-04-22 16:59:49 +0100

    Fix symbolicator port

--- Docker/Converters/SymbolicatorConverter.cs
+++ Docker/Converters/SymbolicatorConverter.cs
@@ -1,4 +1,5 @@
-﻿using SentryOperator.Docker.Volume;
+﻿using k8s.Models;
+using SentryOperator.Docker.Volume;
 using SentryOperator.Entities;
 
 namespace SentryOperator.Docker.Converters;
@@ -6,6 +7,20 @@ namespace SentryOperator.Docker.Converters;
 public class SymbolicatorConverter : ContainerConverter
 {
     public override bool CanConvert(string name, DockerService service) => name == "symbolicator";
+
+    protected override V1Container GetBaseContainer(string name, DockerService service, SentryDeployment sentryDeployment)
+    {
+        var container = base.GetBaseContainer(name, service, sentryDeployment);
+
+        container.Ports ??= new List<V1ContainerPort>();
+        container.Ports.Add(new V1ContainerPort(3021)
+        {
+            Name = "http",
+            Protocol = "TCP"
+        });
+        
+        return container;
+    }
 
     protected override IEnumerable<VolumeRef> GetVolumeData(DockerService service, SentryDeployment sentryDeployment)
     {
