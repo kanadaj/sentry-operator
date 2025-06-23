48fa9ddc6f8311da3a7d2c49be10abb65ab1ebef
Daniel Rusznyak <daniel@rusznyak.com>
2025-06-23 15:10:41 +0100

    Add TaskBrokerConverter to handle taskbroker service conversion

---
+++ Docker/Converters/TaskBrokerConverter.cs
@@ -0,0 +1,21 @@
+﻿using k8s.Models;
+using SentryOperator.Entities;
+
+namespace SentryOperator.Docker.Converters;
+
+public class TaskBrokerConverter : ContainerConverter
+{
+    public override bool CanConvert(string name, DockerService service) => name == "taskbroker";
+
+    protected override V1Container GetBaseContainer(string name, DockerService service, SentryDeployment sentryDeployment)
+    {
+        var container = base.GetBaseContainer(name, service, sentryDeployment);
+        container.Ports ??= new List<V1ContainerPort>();
+        container.Ports.Add(new V1ContainerPort
+        {
+            ContainerPort = 50051,
+            Name = "taskbroker"
+        });
+        return container;
+    }
+}
