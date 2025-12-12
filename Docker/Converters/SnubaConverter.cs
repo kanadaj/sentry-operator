012066e6b7124465bff2d25285c0c5b646fef5cc
Daniel Rusznyak <daniel@rusznyak.com>
2025-12-12 18:58:28 +0000

    Add snuba-env to snuba containers

---
+++ Docker/Converters/SnubaConverter.cs
@@ -0,0 +1,25 @@
+﻿using k8s.Models;
+using SentryOperator.Entities;
+
+namespace SentryOperator.Docker.Converters;
+
+public class SnubaConverter : ContainerConverter
+{
+    public override bool CanConvert(string name, DockerService service) => service.Image?.Contains("getsentry/snuba") ?? false;
+
+    protected override V1PodSpec GeneratePodSpec(string name, DockerService service, SentryDeployment sentryDeployment)
+    {
+        var pod = base.GeneratePodSpec(name, service, sentryDeployment);
+        
+        foreach (var container in pod.Containers)
+        {
+            container.EnvFrom ??= new List<V1EnvFromSource>();
+            container.EnvFrom.Add(new V1EnvFromSource
+            {
+                SecretRef = new V1SecretEnvSource("snuba-env")
+            });
+        }
+        
+        return pod;
+    }
+}
